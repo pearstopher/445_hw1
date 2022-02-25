43f5ad33512d2c934515a096b4fe8599c7b1f537
Pearstopher <pearstopher@gmail.com>
2022-02-25 13:20:16 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# CS445 Homework 1 Problem 11
+# Christopher Juncker
+# This is a sample Python script.
+
+# 11. This is a short coding problem. You will use a perceptron with 785 inputs (including bias input)
+# and 10 outputs to learn to classify the handwritten digits in the MNIST dataset
+# (http://yann.lecun.com/exdb/mnist/). See the class slides for details of the perceptron architecture
+# and perceptron learning algorithm.
+
+
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
